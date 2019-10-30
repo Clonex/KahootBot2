70b26c64f7980d464b6f153dd6ea0498eeb1dc33
Michael <clonex_kontakt@hotmail.com>
2019-10-30 10:38:52 +0100

    Compressed old files

--- oldFiles/oldStart.js
+++
@@ -1,9 +0,0 @@
-const getToken = require('./ManageTokens');
-
-if(process.argv.length > 3)
-{
-    getToken.Start(process.argv[2], process.argv[3]);
-    setInterval(getToken.RequestAction, 20);
-}else{
-    console.log("Arguments needed! Usage:\nyarn start ID amountOfBots");
-}
