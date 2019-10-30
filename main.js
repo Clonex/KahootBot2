ccd195585207789ddaf98426520b2743d0133e41
Michael <clonex_kontakt@hotmail.com>
2019-10-30 02:20:28 +0100

    Cleanup

--- main.js
+++ main.js
@@ -8,12 +8,3 @@ if(process.argv.length > 3)
     yarn start <ID> <amountOfBots> [prefix=""]
     `);
 }
-/*const getToken = require('./ManageTokens');
-
-if(process.argv.length > 3)
-{
-    getToken.Start(process.argv[2], process.argv[3]);
-    setInterval(getToken.RequestAction, 20);
-}else{
-    console.log("Arguments needed! Usage:\nyarn start ID amountOfBots");
-}*/
